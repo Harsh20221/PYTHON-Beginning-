7a7f10e11944b44e2066d80344c20f5df1772bad
Harsh Kumar <harsh20221@gmail.com>
2024-02-02 04:19:22 +0530

    commit

---
+++ Rightangleasterix.py
@@ -0,0 +1,9 @@
+def print_triangle(rows):
+    for i in range(1, rows + 1):
+    
+        print("*" * i)
+
+rows = int(input("Enter the number of rows for the triangle: "))
+
+
+print_triangle(rows)
